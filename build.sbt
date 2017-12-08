bc1090057a2695cad40025d47115b06b19debd81
Rohit Saxena <aerohit.saxena@gmail.com>
2017-12-08 13:47:20 +0100

    Created a frontend server which can talk to backend server.

--- build.sbt
+++ build.sbt
@@ -1,5 +1,7 @@
 import Dependencies._
 
+// TODO: Split these settings into more sensible groups
+// (eg: not every project would be published as docker image)
 lazy val commonSettings = Seq(
   organization := "com.aerohitsaxena",
   version := "0.1.0-SNAPSHOT",
@@ -13,6 +15,20 @@ lazy val commonSettings = Seq(
 lazy val akkaHttpVersion = "10.0.11"
 
 lazy val backend = (project in file("backend"))
+  .settings(
+    commonSettings,
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
+    )
+  )
+  .enablePlugins(JavaAppPackaging)
+  .enablePlugins(DockerPlugin)
+  .enablePlugins(AshScriptPlugin)
+
+// TODO: create micro service kind of plugin for above
+
+lazy val frontend = (project in file("frontend"))
   .settings(
     commonSettings,
     libraryDependencies ++= Seq(
---
+++ frontend/src/main/scala/FrontendServer.scala
@@ -0,0 +1,49 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
+import com.typesafe.config.ConfigFactory
+
+import scala.util.Failure
+import scala.util.Success
+import scala.concurrent.Future
+import akka.http.scaladsl.model._
+
+object FrontendServer extends App {
+
+  import JsonFormats._
+
+  implicit val system           = ActorSystem("frontend-system")
+  implicit val materializer     = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val hostName = java.net.InetAddress.getLocalHost.getHostName
+
+  val config             = ConfigFactory.load()
+  val backendHost        = config.getString("backend.host")
+  val backendHostnameUri = s"$backendHost/hostname"
+  val serverInfo         = ServerResponse(frontendHost = hostName, backendHost = backendHost)
+
+  val route =
+    path("serverinfo") {
+      get {
+        val responseFuture = Http().singleRequest(HttpRequest(uri = backendHostnameUri))
+        onComplete(responseFuture) {
+          case Success(response) => complete(response)
+          case Failure(ex)       => complete(s"An error occurred: ${ex.getMessage}")
+        }
+      }
+    }
+
+  // if exposed "localhost" only requests from docker container would be accepted
+  Http().bindAndHandle(route, "0.0.0.0", 9100)
+
+}
+
+case class ServerResponse(frontendHost: String, backendHost: String)
+
+object JsonFormats extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit lazy val serverInfoFormat: RootJsonFormat[ServerResponse] = jsonFormat2(ServerResponse)
+}
